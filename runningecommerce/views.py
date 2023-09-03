a91ca6f4217a95caa0b3f3d430ca88f47a8857b8
nandishnagaraj <nandish.nagaraj@gmail.com>
2023-09-03 15:53:25 +0530

    adding Training List

--- runningecommerce/views.py
+++ runningecommerce/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from trainingplans.models import TrainingPlans
 
+def home(request):
+    plans = TrainingPlans.objects.all()    
+    context = {
+        'plans': plans,
+    }
+    return render(request, "home.html", context)
 
-def home(request):    
-    return render(request, "home.html")
--- trainingplans/views.py
+++ trainingplans/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from trainingplans.models import TrainingPlans
 
 # Create your views here.
+def trainingplans(request,pk):
+    plan = get_object_or_404(TrainingPlans,pk)
+    print(plan)
+    
